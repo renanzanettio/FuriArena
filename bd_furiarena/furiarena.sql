93a043541e78e364d9a2cd0942abf203a3201204
renanzanettio <renanzanettio@gmail.com>
2025-05-03 13:13:53 -0300

    banco de dados atualizado

--- bd_furiarena/furiarena.sql
+++ bd_furiarena/furiarena.sql
@@ -28,13 +28,14 @@ CREATE TABLE IF NOT EXISTS `campeonato` (
   `status_campeonato` varchar(100) NOT NULL,
   `img_jogo_campeonato` varchar(100) NOT NULL,
   PRIMARY KEY (`id_campeonato`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela furiarena.campeonato: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela furiarena.campeonato: ~4 rows (aproximadamente)
 INSERT INTO `campeonato` (`id_campeonato`, `nome_campeonato`, `jogo_campeonato`, `sigla_jogo_campeonato`, `status_campeonato`, `img_jogo_campeonato`) VALUES
 	(1, 'RLCS 2025 - Raleigh Major: South America Open 4', 'Rocket League', 'Rl', 'Fase de Grupos', 'assets/img/rl-logo.svg'),
 	(2, 'LTA Sul Split 2 2025 - Grupo B', 'League of Legends', 'LOL', 'Fase de Grupos', 'assets/img/lol-logo.svg'),
-	(3, 'PGL Astana 2025', 'Counter Stike 2', 'CS2', 'Aguardando Inicio', 'assets/img/cs2-logo.svg');
+	(3, 'PGL Astana 2025', 'Counter Stike 2', 'CS2', 'Aguardando Inicio', 'assets/img/cs2-logo.svg'),
+	(5, 'VCT 2025: Americas Stage 2', 'Valorant', 'VAL', 'Aguardando Inicio', 'assets/img/valorant-logo.svg');
 
 -- Copiando estrutura para tabela furiarena.partidas
 CREATE TABLE IF NOT EXISTS `partidas` (
@@ -43,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `partidas` (
   `id_time` int(11) NOT NULL DEFAULT 0,
   `id_campeonato` int(11) NOT NULL,
   `hora_campeonato` varchar(5) NOT NULL,
-  `data_campeonato` varchar(5) NOT NULL,
+  `data_campeonato` date NOT NULL,
   `placar_adversario_campeonato` int(11) NOT NULL,
   `placar_furia_campeonato` int(11) NOT NULL,
   `status_partida` varchar(50) NOT NULL,
@@ -53,26 +54,36 @@ CREATE TABLE IF NOT EXISTS `partidas` (
   KEY `FK_partidas_campeonato` (`id_campeonato`),
   CONSTRAINT `FK__times` FOREIGN KEY (`id_time`) REFERENCES `times` (`id_time`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_partidas_campeonato` FOREIGN KEY (`id_campeonato`) REFERENCES `campeonato` (`id_campeonato`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela furiarena.partidas: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela furiarena.partidas: ~8 rows (aproximadamente)
 INSERT INTO `partidas` (`id_partida`, `qtd_partida`, `id_time`, `id_campeonato`, `hora_campeonato`, `data_campeonato`, `placar_adversario_campeonato`, `placar_furia_campeonato`, `status_partida`, `link_partida`) VALUES
-	(1, 3, 2, 2, '15:00', '03/05', 0, 0, 'Aguardando Inicio', 'https://www.twitch.tv/ltasul'),
-	(2, 1, 1, 1, '19:10', '02/05', 0, 0, 'Aguardando Inicio', 'https://www.twitch.tv/rocketleague'),
-	(3, 3, 3, 2, '12:00', '11/05', 0, 0, 'Aguardando Inicio', 'https://www.twitch.tv/ltasul');
+	(1, 3, 2, 2, '15:00', '2025-05-03', 0, 0, 'Aguardando inicio', 'https://www.twitch.tv/ltasul'),
+	(2, 5, 1, 1, '19:10', '2025-05-03', 0, 3, 'Vitória', 'https://www.twitch.tv/rocketleague'),
+	(3, 3, 3, 2, '12:00', '2025-05-03', 0, 0, 'Aguardando inicio', 'https://www.twitch.tv/ltasul'),
+	(4, 5, 5, 1, '20:30', '2025-04-27', 1, 3, 'Vitória', 'https://www.twitch.tv/rocketleague'),
+	(5, 5, 4, 1, '19:55', '2025-04-27', 2, 3, 'Vitória', 'https://www.twitch.tv/rocketleague'),
+	(7, 5, 6, 1, '19:00', '2025-04-27', 1, 3, 'Vitória', 'https://www.twitch.tv/rocketleague'),
+	(8, 5, 7, 1, '19:45', '2025-04-27', 0, 3, 'Vitória', 'https://www.twitch.tv/rocketleague'),
+	(9, 3, 8, 2, '15:00', '2025-04-27', 1, 2, 'Vitória', 'https://www.twitch.tv/ltasul');
 
 -- Copiando estrutura para tabela furiarena.times
 CREATE TABLE IF NOT EXISTS `times` (
   `id_time` int(11) NOT NULL AUTO_INCREMENT,
   `nome_time` varchar(50) NOT NULL,
   PRIMARY KEY (`id_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela furiarena.times: ~3 rows (aproximadamente)
+-- Copiando dados para a tabela furiarena.times: ~8 rows (aproximadamente)
 INSERT INTO `times` (`id_time`, `nome_time`) VALUES
 	(1, 'GRATIA'),
 	(2, 'RED CANIDS'),
-	(3, 'FLUXO W7M');
+	(3, 'FLUXO W7M'),
+	(4, 'AMETHYST'),
+	(5, 'TEAM REDS'),
+	(6, 'PAPO DE VISÃO'),
+	(7, 'RED'),
+	(8, 'LOUD');
 
 -- Copiando estrutura para tabela furiarena.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,113 +1,116 @@
 //Chat
-var socket = io('http://localhost:3000')
+var socket = io("http://localhost:3000");
 
 function renderMessage(message) {
-    $('.grid-chat').append('<div class="message"><label class="text-message"><label class="username">'+ message.author +': </label> ' + message.message + '</label>');
+  $(".grid-chat").append(
+    '<div class="message"><label class="text-message"><label class="username">' +
+      message.author +
+      ": </label> " +
+      message.message +
+      "</label>"
+  );
 }
 
-socket.on('receivedMessage', function(message) {
-    renderMessage(message);
-})
-
-socket.on('previousMessages', function(messages) {
-    for (message of messages) {
-        renderMessage(message);
-    }
-})
-
-$('#chat').submit(function(event) {
-    event.preventDefault();
-
-    var author = $('#userName').text();
-    var message = $('input[name=message]').val();
-
-    console.log(author);
-
-    if (author.length && message.length) {
-        var messageObject = {
-            author: author,
-            message: message,
-        };
-
-        renderMessage(messageObject);
-        socket.emit('sendMessage', messageObject);
-        $('input[name=message]').val('');
-        console.log("enviou");
-    }
+socket.on("receivedMessage", function (message) {
+  renderMessage(message);
 });
 
+socket.on("previousMessages", function (messages) {
+  for (message of messages) {
+    renderMessage(message);
+  }
+});
 
+$("#chat").submit(function (event) {
+  event.preventDefault();
+
+  var author = $("#userName").text();
+  var message = $("input[name=message]").val();
+
+  console.log(author);
+
+  if (author.length && message.length) {
+    var messageObject = {
+      author: author,
+      message: message,
+    };
+
+    renderMessage(messageObject);
+    socket.emit("sendMessage", messageObject);
+    $("input[name=message]").val("");
+    console.log("enviou");
+  }
+});
 
 // Cadastro
-$('#formCadastro').submit(function (e) {
-    e.preventDefault();
-    console.log('Formulário de cadastro enviado!');
-    const data = {
-      nome_usuario: $('input[name=nome_usuario]').val(),
-      email_usuario: $('input[name=email_usuario]').val(),
-      senha_usuario: $('input[name=senha_usuario]').val(),
-    };
-  
-    $.post('/auth/register', data)
-      .done(() => {
-        alert('Usuário cadastrado com sucesso!');
-        console.log('Usuário cadastrado com sucesso!');
-        window.location.href = 'login.html';
-      })
-      .fail(() => {
-        alert('Erro ao cadastrar usuário.');
-        console.log('Erro ao cadastrar usuário.');
-      });
-  });
-  
-  // Login
-  $('#formLogin').submit(function (e) {
-    e.preventDefault();
-    const data = {
-      email_usuario: $('input[name=email_usuario]').val(),
-      senha_usuario: $('input[name=senha_usuario]').val(),
-    };
-  
-    $.post('/auth/login', data)
-      .done(() => {
-        alert('Login realizado com sucesso!');
-        window.location.href = 'chat.html';
-      })
-      .fail(() => {
-        alert('Email ou senha incorretos.');
-      });
-  });
+$("#formCadastro").submit(function (e) {
+  e.preventDefault();
+  console.log("Formulário de cadastro enviado!");
+  const data = {
+    nome_usuario: $("input[name=nome_usuario]").val(),
+    email_usuario: $("input[name=email_usuario]").val(),
+    senha_usuario: $("input[name=senha_usuario]").val(),
+  };
 
-  // LogOut
-  $('.exit').click(function () {
-    window.location.href = '/auth/logout';
-  });
+  $.post("/auth/register", data)
+    .done(() => {
+      alert("Usuário cadastrado com sucesso!");
+      console.log("Usuário cadastrado com sucesso!");
+      window.location.href = "login.html";
+    })
+    .fail(() => {
+      alert("Erro ao cadastrar usuário.");
+      console.log("Erro ao cadastrar usuário.");
+    });
+});
 
-  //Busca o nome e exibe no id userName
-  $(document).ready(function () {
-    $.get('/auth/user')
-      .done(function (res) {
-        $('#userName').text(res.nome); 
-      })
-      .fail(function () {
-        $('#userName').text('Visitante');
-      });
-  });
-  
+// Login
+$("#formLogin").submit(function (e) {
+  e.preventDefault();
+  const data = {
+    email_usuario: $("input[name=email_usuario]").val(),
+    senha_usuario: $("input[name=senha_usuario]").val(),
+  };
 
-  // CRONOGRAMA DE CAMPEONATO
+  $.post("/auth/login", data)
+    .done(() => {
+      alert("Login realizado com sucesso!");
+      window.location.href = "chat.html";
+    })
+    .fail(() => {
+      alert("Email ou senha incorretos.");
+    });
+});
 
-  document.addEventListener("DOMContentLoaded", async () => {
-    const container = document.getElementById("grid-campeonato");
-  
-    try {
-      const res = await fetch("/api/cronograma-campeonatos");
-      const data = await res.json();
-  
-      container.innerHTML = ""; // limpa os cards fixos
-  
-      data.forEach(item => {
-        const card = `
+// LogOut
+$(".exit").click(function () {
+  window.location.href = "/auth/logout";
+});
+
+//Busca o nome e exibe no id userName
+$(document).ready(function () {
+  $.get("/auth/user")
+    .done(function (res) {
+      $("#userName").text(res.nome);
+    })
+    .fail(function () {
+      $("#userName").text("Visitante");
+    });
+});
+
+// CRONOGRAMA DE CAMPEONATO
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.getElementById("grid-campeonato");
+
+  try {
+    const res = await fetch("/api/cronograma-campeonatos");
+    const data = await res.json();
+
+    container.innerHTML = ""; // limpa os cards fixos
+
+    data.forEach((item) => {
+      const card = `
             <div class="campeonato">
               <div class="jogo-campeonato">${item.jogo_campeonato}</div>
               <div class="nome-fase-campeonato">
@@ -117,15 +120,14 @@ $('#formCadastro').submit(function (e) {
               <hr class="hr">
             </div>
         `;
-        container.innerHTML += card;
-      });
-  
-    } catch (err) {
-      console.error("Erro ao carregar cronograma de campeonatos:", err);
-    }
-  });
-  
-  // CRONOGRAMA DE JOGOS
+      container.innerHTML += card;
+    });
+  } catch (err) {
+    console.error("Erro ao carregar cronograma de campeonatos:", err);
+  }
+});
+
+// CRONOGRAMA DE JOGOS
 
 // Função que carrega os jogos de acordo com o valor da radio
 async function carregarJogos(jogoSelecionado) {
@@ -136,18 +138,17 @@ async function carregarJogos(jogoSelecionado) {
     const data = await res.json();
 
     container.innerHTML = "";
-
-    data.forEach(item => {
-      
+    var existe = false;
+    data.forEach((item) => {
       //compara as linhas da query com o valor do jogo selecionado
       if (item.jogo_campeonato === jogoSelecionado) {
-        exite = true
+        existe = true;
         const card = `
           <div class="container-jogo">
             <div class="datahora-times">
               <div class="data-hora-jogo">
                 <label class="hora">${item.hora_campeonato}</label>
-                <label class="data">${item.data_campeonato}</label>
+                <label class="data">${item.data_campeonato[8]}${item.data_campeonato[9]}/${item.data_campeonato[5]}${item.data_campeonato[6]}</label>
               </div>
               <div class="time-campeonato-jogo">
                 <label class="time">
@@ -159,6 +160,7 @@ async function carregarJogos(jogoSelecionado) {
               </div>
             </div>
             <div class="play-partidas-jogo">
+              <div class="status-partida">${item.status_partida}</div>
               <a href="${item.link_partida}"><iconify-icon icon="mdi:play" class="play"></iconify-icon></a>
               <div class="partidas-jogo">
                 <img src="${item.img_jogo_campeonato}" alt="Logo do Jogo">
@@ -177,7 +179,7 @@ async function carregarJogos(jogoSelecionado) {
             <div class="datahora-times">
               <div class="data-hora-jogo">
                 <label class="hora">${item.hora_campeonato}</label>
-                <label class="data">${item.data_campeonato}</label>
+                <label class="data">${item.data_campeonato[8]}${item.data_campeonato[9]}/${item.data_campeonato[5]}${item.data_campeonato[6]}</label>
               </div>
               <div class="time-campeonato-jogo">
                 <label class="time">
@@ -188,7 +190,9 @@ async function carregarJogos(jogoSelecionado) {
                 <label class="campeonato">${item.nome_campeonato}</label>
               </div>
             </div>
+            
             <div class="play-partidas-jogo">
+              <div class="status-partida">${item.status_partida}</div>
               <a href="${item.link_partida}"><iconify-icon icon="mdi:play" class="play"></iconify-icon></a>
               <div class="partidas-jogo">
                 <img src="${item.img_jogo_campeonato}" alt="Logo do Jogo">
@@ -200,7 +204,9 @@ async function carregarJogos(jogoSelecionado) {
         container.innerHTML += card;
       }
     });
-
+    if(existe == false && jogoSelecionado != "todos"){
+      container.innerHTML = '<br><br><label class="sem-jogos">Sem jogos Recentes</label>'
+    }
   } catch (err) {
     console.error("Erro ao carregar cronograma de jogos:", err);
   }
@@ -210,7 +216,7 @@ async function carregarJogos(jogoSelecionado) {
 document.addEventListener("DOMContentLoaded", () => {
   const radios = document.querySelectorAll('input[name="jogo"]');
 
-  radios.forEach(radio => {
+  radios.forEach((radio) => {
     radio.addEventListener("change", () => {
       const selectedValue = radio.value;
       carregarJogos(selectedValue);
@@ -218,25 +224,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// EXECUTAR ASSIM QUE INICIAR A PAGINA
 
-  // EXECUTAR ASSIM QUE INICIAR A PAGINA
-  
-  document.addEventListener("DOMContentLoaded", async () => {
-    const container = document.getElementById("grid-jogos");
-  
-    try {
-      const res = await fetch("/api/cronograma-jogos");
-      const data = await res.json();
-  
-      container.innerHTML = "";
-  
-      data.forEach(item => {
-        const card = `
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.getElementById("grid-jogos");
+
+  try {
+    const res = await fetch("/api/cronograma-jogos");
+    const data = await res.json();
+
+    container.innerHTML = "";
+
+    data.forEach((item) => {
+      const card = `
             <div class="container-jogo">
               <div class="datahora-times">
                 <div class="data-hora-jogo">
                   <label class="hora">${item.hora_campeonato}</label>
-                  <label class="data">${item.data_campeonato}</label>
+                  <label class="data">${item.data_campeonato[8]}${item.data_campeonato[9]}/${item.data_campeonato[5]}${item.data_campeonato[6]}</label>
                 </div>
                 <div class="time-campeonato-jogo">
                   <label class="time">
@@ -248,6 +253,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 </div>
               </div>
               <div class="play-partidas-jogo">
+                <div class="status-partida">${item.status_partida}</div>
                 <a href="${item.link_partida}"><iconify-icon icon="mdi:play" class="play"></iconify-icon></a>
                 <div class="partidas-jogo">
                   <img src="${item.img_jogo_campeonato}" alt="Logo do LOL">
@@ -256,13 +262,9 @@ document.addEventListener("DOMContentLoaded", () => {
               </div>
             </div>
         `;
-        container.innerHTML += card;
-      });
-  
-    } catch (err) {
-      console.error("Erro ao carregar cronograma de jogos:", err);
-    }
-  });
-  
-  
-  
+      container.innerHTML += card;
+    });
+  } catch (err) {
+    console.error("Erro ao carregar cronograma de jogos:", err);
+  }
+});
